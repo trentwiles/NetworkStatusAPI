5887b9bf65a25ce154c825dc45d4366f3e7be644
RiversideRocks <wilest7@gmail.com>
2023-12-30 17:50:54 -0500

    basic testing

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const axios = require('axios');
 const env = require('dotenv')
+const test = require('node:test');
+const assert = require('assert');
 
 env.config()
 
@@ -8,7 +10,16 @@ var password = process.env.TOR_API_PASSWORD
 
 async function torRoute(path){
     const blah = await axios.get(`https://metrics-api.torproject.org${path}`, {auth: { username: username, password: password }});
-    console.log(blah)
+    return blah.data
 }
 
-torRoute("/details?type=relay&running=true")
+
+test('version test', async (t) => {
+    const r =  await torRoute("/details") 
+    assert.strictEqual("tor-0.4.8.10",  r["version"]);
+});
+
+test('build version test', async (t) => {
+    const r =  await torRoute("/details") 
+    assert.strictEqual("0.4.8.10",  r["build_version"]);
+});
